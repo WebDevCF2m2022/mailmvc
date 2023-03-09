f75703f00dfb0920fc10b58739f3164c970f896d
mikhawa <michaeljpitz@gmail.com>
2023-03-09 11:02:39 +0100

    connect to private ok

--- controller/publicController.php
+++ controller/publicController.php
@@ -1,3 +1,18 @@
 <?php
 
+if(isset($_POST['username'],$_POST['password'])){
+    $user = htmlspecialchars(strip_tags(trim($_POST['username'])),ENT_QUOTES);
+    $pwd = htmlspecialchars(strip_tags(trim($_POST['password'])),ENT_QUOTES);
+
+    $connect = connectUsers($PDOConnect,$user,$pwd);
+
+    if(is_string($connect)){
+        $message = $connect;
+    }else{
+        header("Location: ./");
+        exit();
+    }
+
+}
+
 require_once "../view/publicView.php";
--- model/UsersModel.php
+++ model/UsersModel.php
@@ -7,25 +7,41 @@
  * @param string $pwd
  * @return bool|string
  */
-function connectUsers(PDO $c, string $login, string $pwd): bool|string{
-    $sql = "SELECT * FROM users WHERE username = ?";
+function connectUsers(PDO $c, string $login, string $pwd): bool|string
+{
+    $sql = "SELECT * FROM users WHERE usersname = ?";
     $query = $c->prepare($sql);
-    try{
+    try {
         $query->execute([$login]);
-    }catch (Exception $e){
+    } catch (Exception $e) {
         return $e->getMessage();
     }
-    if($query->rowCount()===1){
+    if ($query->rowCount() === 1) {
         $response = $query->fetch(PDO::FETCH_ASSOC);
-        if(password_verify($pwd,$response['userpwd'])){
-            $_SESSION[]=$response;
+        if (password_verify($pwd, $response['userspwd'])) {
+            $_SESSION[] = $response;
             unset($_SESSION['userspwd']);
             $_SESSION['myID'] = session_id();
             return true;
-        }else{
+        } else {
             return "Login ou mot de passe incorrect 2";
         }
-    }else{
+    } else {
         return "Login ou mot de passe incorrect 1";
     }
+}
+
+function disconnectUsers():bool{
+    $_SESSION = [];
+
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+
+    session_destroy();
+    return true;
 }

6c8696fc79a69856a88357c29e1f853ec38aa3fb
mikhawa <michaeljpitz@gmail.com>
2023-03-10 11:09:23 +0100

    test PDO ok

--- public/index.php
+++ public/index.php
@@ -2,16 +2,17 @@
 # PHP SESSION CONNECT
 session_start();
 
-# PHP ini_set mail
-ini_set('SMTP', MAIL_SERVER);
-ini_set('smtp_port', MAIL_PORT);
-ini_set('sendmail_from', MAIL_FROM);
 
 # Dependencies
 require_once "../config.php"; # DB
 require_once "../model/UsersModel.php"; # table users
 require_once "../model/MessagesModel.php"; # table messages
 require_once "../model/MailModel.php"; # send mail
+
+# PHP ini_set mail
+ini_set('SMTP', MAIL_SERVER);
+ini_set('smtp_port', MAIL_PORT);
+ini_set('sendmail_from', MAIL_FROM);
 
 
 # Connexion
